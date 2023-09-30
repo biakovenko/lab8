1b01a697e8163fdf4d8b54a566cff2a53c9d28c1
biakovenko <zbogdanyakovenko@gmail.com>
2023-09-30 11:13:52 +0300

    Изменил код

--- hello.pas
+++ hello.pas
@@ -1,3 +1,13 @@
 ﻿begin
-  Print('Привет, мир!');
+  var t := readinteger('Какой сейчас час?');
+  if (t >= 4) and (t <= 10) then
+    println('Доброе утро, мир!')
+  else
+  if (t >= 11) and (t <= 16) then
+    println('Добрый день, мир!')
+  else
+  if (t >= 17) and (t <= 22) then
+    println('Добрый вечер, мир!')
+  else
+    println('Доброй ночи, мир!');
 end.
